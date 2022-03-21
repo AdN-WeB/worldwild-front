edbfb4966997ab39e9b31127611a08e0b8172ade
adrien <adrien.mervelay84@gmail.com>
2022-03-21 16:31:42 +0100

    ajout des routes sur les boutons de home + component plants + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,35 @@
+import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalsComponent } from './animals/animals.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { PlantsComponent } from './plants/plants.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo:'home',
-    pathMatch:'full'
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
-    path:'home',
-    component:HomeComponent
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'animals',
-    component:AnimalsComponent
+    component: AnimalsComponent,
   },
+  { path: 'plants',
+    component: PlantsComponent },
   {
     path: 'dashboard',
-    component: DashboardComponent
-  }
+    component: DashboardComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
